d9c4ab65ac173c5dbaf3e7178bf34999eb659874
fonglh <fonglh@gmail.com>
2018-08-01 01:12:18 +0800

    Add SQL command for resale_transactions table.

--- schema.sql
+++ schema.sql
@@ -2,3 +2,5 @@
 
 CREATE TABLE blocks(postal_code INT, address TEXT, town_name TEXT, town_code TEXT, lease_commenced_date DATE, lease_remaining INT, lease_period INT, building_gl INT);
 ALTER TABLE blocks ADD PRIMARY KEY(postal_code);
+
+CREATE TABLE resale_transactions(postal_code INT, resale_price DECIMAL, floor_area DECIMAL, floor_min INT, floor_max INT, registration_date DATE, model TEXT, flat_type TEXT, remaining_lease INT);
