e42692561045053c435e26d6fed21586c07c8dc8
fonglh <fonglh@gmail.com>
2018-08-01 22:32:48 +0800

    Add primary key to resale_transactions table.

--- schema.sql
+++ schema.sql
@@ -4,3 +4,4 @@ CREATE TABLE blocks(postal_code INT, address TEXT, town_name TEXT, town_code TEX
 ALTER TABLE blocks ADD PRIMARY KEY(postal_code);
 
 CREATE TABLE resale_transactions(postal_code INT, resale_price DECIMAL, floor_area DECIMAL, floor_min INT, floor_max INT, registration_date DATE, model TEXT, flat_type TEXT, remaining_lease INT);
+ALTER TABLE resale_transactions ADD PRIMARY KEY(postal_code, registration_date, resale_price);
